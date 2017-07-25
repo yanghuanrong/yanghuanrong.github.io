54c996ca806c127d7dc2f5727a95efd291dc571f
yanghuanrong <1176989241@qq.com>
2017-07-25 15:08:40 +0800

    blog

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../blog/index.html'),
     assetsRoot: path.resolve(__dirname, '../blog'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: './static',
     assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
