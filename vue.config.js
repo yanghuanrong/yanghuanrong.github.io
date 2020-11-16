d8b7337a1971b51703343b2c71112b30f9b5a54e
yanghuanrong <1176989241@qq.com>
2020-11-16 17:23:48 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
     // 扩展 webpack 配置
+    publicPath: process.env.NODE_ENV === "production" ? "/vue-running" : "/",
     chainWebpack: config => {
         config.resolve.alias
             .set('@blog', path.resolve('blog'));
