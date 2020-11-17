38fee06f036a8c04a6d19a8748a37ebb022d4c5c
yanghuanrong <1176989241@qq.com>
2020-11-17 08:50:28 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# 确保脚本抛出遇到的错误
+set -e
+
+git add .
+git commit -am 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名
+git push -f git@github.com:yanghuanrong/yanghuanrong.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
