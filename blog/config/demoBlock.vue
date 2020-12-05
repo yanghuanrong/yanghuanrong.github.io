069aba18510d37830e74bca27d2432391027d106
yanghuanrong <1176989241@qq.com>
2020-12-05 21:02:54 +0800

    deploy

--- blog/config/demoBlock.vue
+++ blog/config/demoBlock.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="demo-block">
-    <div class="splitter">
-      <span>demo</span>
-      <hr />
-    </div>
     <div class="source">
       <slot name="source"></slot>
     </div>
@@ -21,12 +17,26 @@
 <script type="text/babel">
 import hljs from "highlight.js";
 import "highlight.js/styles/atom-one-dark.css";
+import "./demoBlock.less";
 
 export default {
   mounted() {
     this.$nextTick(() => {
       const blocks = document.querySelectorAll("pre code:not(.hljs)");
       Array.prototype.forEach.call(blocks, hljs.highlightBlock);
+
+      const elements = document.querySelectorAll("pre code.hljs");
+      for (let i = 0; i < elements.length; i++) {
+        let html = elements[i].innerHTML;
+        let num = 1;
+        html = '<span class="ln-num" data-num="' + num + '"></span>' + html;
+        html = html.trim().replace(/\r\n|\r|\n/g, function (a) {
+          num++;
+          return a + '<span class="ln-num" data-num="' + num + '"></span>';
+        });
+        html = '<span class="ln-bg"></span>' + html;
+        elements[i].innerHTML = html;
+      }
     });
   },
 };
