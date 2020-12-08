eacdf76745fc6b0b90aa43c299bc1976918fc099
qq1176989241 <1176989241@qq.com>
2020-12-08 17:55:57 +0800

    edit

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,7 @@ git commit -am 'deploy'
 git push -u origin dev
 git checkout master
 git merge dev
-
-# 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名
 git push -u origin master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git checkout dev
 
 cd -
